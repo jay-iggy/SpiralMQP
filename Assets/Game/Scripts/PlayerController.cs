03cff38d8cf607bd9ea57b9cd85e75d1d8a231c5
SamuelWilensky <Samuelwilensky@gmail.com>
2024-09-08 15:18:41 -0400

    lerp movement

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
+    [SerializeField] float turningRadius;
+    [SerializeField] GameObject fist;
     private Vector2 direction = new Vector2();
     private Rigidbody rb;
 
     public PlayerInput playerControls;
     private InputAction move;
+    private InputAction fire;
+
+    bool punch = false;
 
     private void Awake()
     {
@@ -21,11 +27,15 @@ public class PlayerController : MonoBehaviour
     {
         move = playerControls.Player.Move;
         move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
     }
 
     private void OnDisable()
     {
         move.Disable();
+        fire.Disable();
     }
 
     void Start()
@@ -36,11 +46,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction = move.ReadValue<Vector2>();
+        
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(direction.x*movementSpeed, 0, direction.y*movementSpeed);
+        Vector3 targetVelocity = new Vector3(direction.x * movementSpeed, 0, direction.y * movementSpeed);
+        rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, Time.deltaTime * turningRadius);
     }
 
 }
