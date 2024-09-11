6c9df20c0514ab91c6bd3d6819a352596ddd191b
cu5tin <cu5tin@outlook.com>
2024-09-10 22:39:28 -0400

    hit jiggle

---
+++ Assets/Game/Scripts/HitJiggle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Game.Scripts {
+    public class HitJiggle : MonoBehaviour {
+        public AnimationCurve jiggleCurve;
+        private Vector3 originalScale;
+        
+        private void Start() {
+            originalScale = transform.localScale;
+        }
+
+        public void Jiggle() {
+            StartCoroutine(JiggleCoroutine());
+        }
+
+        private IEnumerator JiggleCoroutine() {
+            float timer = 0;
+            float curveDuration = jiggleCurve.keys[jiggleCurve.length - 1].time;
+            
+            while(timer < curveDuration) {
+                timer += Time.deltaTime;
+                float curveValue = jiggleCurve.Evaluate(timer);
+                
+                transform.localScale = originalScale * curveValue;
+                
+                yield return null;
+            }
+        }
+    }
+}
