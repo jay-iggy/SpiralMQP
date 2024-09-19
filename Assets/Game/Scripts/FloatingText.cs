dc72760d60629d4cc08c3d97cd90bd6a71246b63
cu5tin <cu5tin@outlook.com>
2024-09-19 13:08:54 -0400

    damage text refactoring

--- Assets/Game/Scripts/FloatingText.cs
+++ Assets/Game/Scripts/FloatingText.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class FloatingText : MonoBehaviour
 {
-    public float DestroyTime = 3f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Destroy(gameObject,DestroyTime);
+    public float destroyDelay = 3f;
+
+    void Start() {
+        Destroy(gameObject,destroyDelay);
     }
-
-
 }
--- Assets/Game/Scripts/HealthComponent.cs
+++ Assets/Game/Scripts/HealthComponent.cs
@@ -1,22 +1,32 @@
-﻿using Game.Scripts.Interfaces;
+﻿using System;
+using Game.Scripts.Interfaces;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using UnityEngine.Serialization;
 
 namespace Game.Scripts {
     public class HealthComponent : MonoBehaviour, ICanGetHit {
         public UnityEvent<float> onHealthChanged;
         public UnityEvent onDeath;
         public UnityEvent onTakeDamage;
+        public UnityEvent<float> onTakeDamageFloat;
 
         //Damage Display Stuff
-        public GameObject FlotingTextPrefab;
+        public GameObject floatingTextPrefab;
 
         public float health { get; private set; }
         public float maxHealth = 100;
         
         private void Awake() {
             health = maxHealth;
+        }
+
+        private void Start() {
+            if(floatingTextPrefab != null) {
+                onTakeDamageFloat.AddListener(ShowFloatingText);
+            }
+            onDeath.AddListener(PlayDeathJuice);
         }
 
         public void SetHealth(float newHealth) {
@@ -26,22 +36,10 @@ namespace Game.Scripts {
         public void TakeDamage(float damage) {
             SetHealth(health - damage);
             onTakeDamage.Invoke();
-
-            Debug.Log("TakenDamage()");
-
-            //Display Damage taken
-            if (FlotingTextPrefab != null){
-                Debug.Log("if exict");
-                ShowFloatingText();
-            }
-           
+            onTakeDamageFloat.Invoke(damage);
 
             if (health <= 0) {
                 onDeath.Invoke();
-                
-                // TODO: move this out of here
-                ScreenShake.instance.StartShake(0.5f, 0.5f);
-                HitPause.instance.Pause(0.35f);
             }
         }
 
@@ -50,12 +48,16 @@ namespace Game.Scripts {
             Debug.Log(damage + " damage taken!");
         }
 
-        public void ShowFloatingText()
-        {
-            Debug.Log("show floatingtext");
-            //Creat text
-            var go = Instantiate(FlotingTextPrefab, transform.position,Quaternion.identity,transform);
-            go.GetComponent<TMP_Text>().text = "1"; //set value, Currently only show 1
+        private void ShowFloatingText(float damage) {
+            GameObject textObj = Instantiate(floatingTextPrefab);
+            textObj.transform.position = transform.position;
+            
+            textObj.GetComponent<TMP_Text>().SetText($"{damage}"); //set value, Currently only show 1
+        }
+
+        private void PlayDeathJuice() {
+            ScreenShake.instance.StartShake(0.5f, 0.5f);
+            HitPause.instance.Pause(0.35f);
         }
     }
 }
