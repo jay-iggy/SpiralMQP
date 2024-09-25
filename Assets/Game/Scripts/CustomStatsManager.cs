ff60bf91c9fc2e740592ece8f33803036e67e365
cu5tin <cu5tin@outlook.com>
2024-09-25 02:04:03 -0400

    custom stats

---
+++ Assets/Game/Scripts/CustomStatsManager.cs
@@ -0,0 +1,24 @@
+﻿using Game.Scripts.Analytics;
+using UnityEngine;
+
+namespace Game.Scripts {
+    public class CustomStatsManager : MonoBehaviour {
+        
+        
+        public void ResetCustomStats() {
+            AnalyticsManager.instance.IncrementPlayerNum();
+        }
+    }
+    
+    public struct CustomStats {
+        public int playerHealth;
+        public float playerSpeed;
+        public float playerAttackSpeed;
+        public int enemyHealthMult;
+        public float enemyAttackSpeedMult;
+        
+        public string[] ToArray() {
+            return new string[] {playerHealth.ToString(), playerSpeed.ToString(), playerAttackSpeed.ToString(), enemyHealthMult.ToString(), enemyAttackSpeedMult.ToString() };
+        }
+    }
+}
