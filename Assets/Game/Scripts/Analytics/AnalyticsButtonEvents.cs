6795bc63fbdbe1b608140e96c9e395349a8f5431
cu5tin <cu5tin@outlook.com>
2024-09-28 18:57:01 -0400

    new player button on main menu: increments player num and resets stats

---
+++ Assets/Game/Scripts/Analytics/AnalyticsButtonEvents.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Analytics {
+    public class AnalyticsButtonEvents : MonoBehaviour {
+        public void LoadNewPlayer() {
+            AnalyticsManager.instance.IncrementPlayerNum();
+            CustomStatsManager.instance.ResetCustomStats();
+        }
+    }
+}
