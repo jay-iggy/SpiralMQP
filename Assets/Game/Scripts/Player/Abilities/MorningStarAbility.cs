dbd947c92521ad1080a08bcaa53942f244fbc4b7
H4C1M <micahvargas@icloud.com>
2024-11-07 12:36:01 -0500

    Morning Star item

---
+++ Assets/Game/Scripts/Player/Abilities/MorningStarAbility.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using Game.Scripts.Interfaces;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Game.Scripts.Abilities {
+    public class MorningStarAbility : Ability {
+        [Header("Melee")]
+        [SerializeField] GameObject prefabMStar;
+        private GameObject mStar;
+        [SerializeField] Vector3 mStarOffset;
+        [SerializeField] float mStarCooldown;
+        [SerializeField] float mStarDuration;
+        private float _mStarTimer = 0;
+        public float dmg = 2;
+        public float knockback = 10;
+        private float speed;
+        
+        private void Start() {
+
+            Quaternion startingRotation = prefabMStar.transform.rotation;
+
+            mStar = Instantiate(prefabMStar, this.transform.position + mStarOffset, prefabMStar.transform.rotation, this.transform);
+
+            Transform reticle = this.transform.parent.parent.parent.GetChild(2);
+            Quaternion toRotation = Quaternion.LookRotation(reticle.position - mStar.transform.position, Vector3.up);
+            mStar.transform.eulerAngles = new Vector3(90, toRotation.eulerAngles.y, 0);
+
+            if (mStar.TryGetComponent(out Hitbox hitbox)) {
+                BindHitbox(hitbox);
+            }
+
+            // mStarCooldown /= CustomStatsManager.instance.customStats.playerAttackSpeed;
+            // mStarCooldown *= CustomStatsManager.instance.customStats.playerAttackSpeed;
+        }
+        private void Update()
+        {
+            Transform reticle = this.transform.parent.parent.parent.GetChild(2);
+            Quaternion toRotation = Quaternion.LookRotation(reticle.position - mStar.transform.position, Vector3.up);
+            mStar.transform.eulerAngles = new Vector3(90, toRotation.eulerAngles.y, 0);
+        }
+
+        void BindHitbox(Hitbox hitbox) {
+            hitbox.onHitTarget.AddListener(ProcessAttack);
+        }
+        private void ProcessAttack(ICanGetHit hurtbox) {
+            hurtbox.GetHit(dmg);
+            
+            // knockback the target
+            if(hurtbox is MonoBehaviour target) {
+                Vector3 direction = target.transform.position - transform.position;
+                direction.y = 0;
+                direction.Normalize();
+                target.GetComponent<MovementComponent>().AddExternalVelocity(direction * knockback);
+            }
+        }
+        
+        public override void AbilityPressed() {
+            if (_mStarTimer > 0) {
+                return;
+            }
+            mStar.transform.position = this.transform.position;
+            mStar.SetActive(true);
+            _mStarTimer = mStarCooldown + mStarDuration;
+            
+            StartCoroutine(ResetPunchTimer());
+        }
+        public override void AbilityReleased() {
+            // nothing to do here
+        }
+        
+        private IEnumerator ResetPunchTimer() {
+            while (_mStarTimer > 0) {
+                _mStarTimer -= Time.deltaTime;
+                yield return null;
+            }
+            mStar.SetActive(false);
+        }
+        
+        void OnTriggerEnter(Collider other) {
+            if(other.gameObject.CompareTag(TagManager.Enemy)) { // TODO: make this based off hitbox tagsToHit
+                Vector3 direction = other.transform.position - transform.position;
+                direction.y = 0;
+                _player.movementComponent.AddPersonalVelocity(direction * 1);
+            }
+        }
+    }
+}
