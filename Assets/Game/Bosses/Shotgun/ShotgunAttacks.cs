af19d6f9aae255f99c5ae2e930a7b328f15aad9d
cu5tin <cu5tin@outlook.com>
2024-12-19 09:05:23 -0500

    reorganized ShotgunAttacks.cs for readability
    added regions, moved functions around

--- Assets/Game/Bosses/Shotgun/ShotgunAttacks.cs
+++ Assets/Game/Bosses/Shotgun/ShotgunAttacks.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 namespace Game.Scripts {
-    public class ShotgunAttacks : MonoBehaviour, ICanAttack {
+    public class ShotgunAttacks : MonoBehaviour {
         [Header("Movement")]
         [SerializeField] private float speed = 10;
         [SerializeField] private float moveDistanceThreshold = 3;
@@ -45,30 +45,39 @@ namespace Game.Scripts {
             _player = GameObject.FindGameObjectWithTag(TagManager.Player); // expensive, we can just make the player a singleton
             StartCoroutine(ShotgunEnemyBehavior());
         }
-
         private void OnEnable() {
             _healthComponent.onHealthChanged.AddListener(OnHealthChanged);
         }
-
+        private void OnDestroy() {
+            foreach (GameObject item in _itemsToCleanup) {
+                if(item != null) {
+                    Destroy(item);
+                }
+            }
+        }
+        
         private void Update() {
             _animator.SetFloat("WalkSpeed", _movementComponent.moveVelocity.magnitude);
         }
         
-        public int GetAttackCount() { return 1; }
-
-        public float Attack(int index) {
-            return 0;
-        }
-
-        private IEnumerator ShotgunEnemyBehavior() {
-            print("ShotgunEnemyBehavior started");
-            while (true) {
-                yield return MoveToPoint(BossRoom.GetRandomPositionInRoom(5));
-                yield return Attack_Shoot();
-                yield return Attack_EjectShells();
-                yield return new WaitForSeconds(1);
+        #region Behavior
+            private IEnumerator ShotgunEnemyBehavior() {
+                print("ShotgunEnemyBehavior started");
+                while (true) {
+                    yield return MoveToPoint(BossRoom.GetRandomPositionInRoom(5));
+                    yield return Attack_Shoot();
+                    yield return Attack_EjectShells();
+                    yield return new WaitForSeconds(1);
+                }
             }
-        }
+            private void OnHealthChanged(float newHealth) {
+                // enter phase 2 if less than half health
+                if (newHealth/_healthComponent.maxHealth < .5f) {
+                    volleysPerAttack = 6;
+                    _healthComponent.onHealthChanged.RemoveListener(OnHealthChanged); // prevent phase changing again
+                }
+            }
+        #endregion
         
         private IEnumerator MoveToPoint(Vector3 point) {
             while (Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), point) > moveDistanceThreshold) {
@@ -83,93 +92,72 @@ namespace Game.Scripts {
             }
             _movementComponent.moveVelocity = Vector3.zero;
         }
-        
-        
-        private IEnumerator Attack_EjectShells() {
-            _animator.SetTrigger("EjectShells");
-            yield return new WaitForSeconds(ejectShellsAnim.length);
-        }
-        public void EjectShells() { // this is invoked by animation event
-            for (int i = 0; i < volleysPerAttack; i+=2) {
-                foreach (Transform ejectPos in shellEjectPositions) {
-                    GameObject shell = Instantiate(shellPrefab, ejectPos.position, shellPrefab.transform.rotation);
-                    shell.GetComponent<Rigidbody>().AddForce(ejectPos.forward * shellEjectForce, ForceMode.Impulse);
-                    _itemsToCleanup.Add(shell);
-                }
-            }
-        }
-        
-        
-        
-        private IEnumerator RotateToFacePlayer() {
-            float timer=0;
-            while (Vector3.Angle(transform.forward, _player.transform.position - transform.position) > 10f){
-                Vector3 targetDir = _player.transform.position - transform.position;
-                float step = aimRotateSpeed * Time.deltaTime;
-                Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-                transform.rotation = Quaternion.LookRotation(newDir);
-                Vector3 origRot = transform.eulerAngles;
-                float x;
-                if (_lockXRotation) {
-                    x = 0;
-                } else {
-                    x = origRot.x;
-                }
-                transform.eulerAngles = new Vector3(x, transform.eulerAngles.y, origRot.z);
-                yield return null;
-                if (timer > -1) {
-                    timer += Time.deltaTime;
-                }
-                if (timer > maxAimTime) {
-                    _animator.SetTrigger("JumpShot");
-                    timer = -1;
-                    _fired = true;
-                }
-            }
-            _lockXRotation = true;
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-        }
-        
-        public void UnlockXRotation() {
-            _lockXRotation = false;
-        }
 
-        private IEnumerator Attack_Shoot() {
-            for(int i = 0; i < volleysPerAttack; i++) {
-                _fired = false;
-                yield return RotateToFacePlayer();
-                if(!_fired) {
-                    _animator.SetTrigger("Shoot");
-                    yield return new WaitForSeconds(shootAnim.length + delayBetweenVolleys);
+        #region Shell Eject Attack
+            private IEnumerator Attack_EjectShells() {
+                _animator.SetTrigger("EjectShells");
+                yield return new WaitForSeconds(ejectShellsAnim.length);
+            }
+            public void EjectShells() { // this is invoked by animation event
+                for (int i = 0; i < volleysPerAttack; i+=2) {
+                    foreach (Transform ejectPos in shellEjectPositions) {
+                        GameObject shell = Instantiate(shellPrefab, ejectPos.position, shellPrefab.transform.rotation);
+                        shell.GetComponent<Rigidbody>().AddForce(ejectPos.forward * shellEjectForce, ForceMode.Impulse);
+                        _itemsToCleanup.Add(shell);
+                    }
                 }
             }
-        }
-        
-        public void ShootVolley() { // this is invoked by animation event
-            GameObject proj = Instantiate(projPrefab, projSpawnPos.position, projSpawnPos.rotation);
-            _itemsToCleanup.Add(proj);
-            _movementComponent.AddExternalVelocity(transform.forward * -knockbackForce);
-        }
-        
-        public void Jump() { // this is invoked by animation event
-            _movementComponent.AddVerticalVelocity(jumpForce);
-        }
-        
-        private void OnHealthChanged(float newHealth) {
-            // enter phase 2 if less than half health
-            if (newHealth/_healthComponent.maxHealth < .5f) {
-                volleysPerAttack = 6;
-                _healthComponent.onHealthChanged.RemoveListener(OnHealthChanged); // prevent phase changing again
-            }
-        }
+        #endregion
 
-        private void OnDestroy() {
-            foreach (GameObject item in _itemsToCleanup) {
-                if(item != null) {
-                    Destroy(item);
+        #region Shooting
+            private IEnumerator Attack_Shoot() {
+                for(int i = 0; i < volleysPerAttack; i++) {
+                    _fired = false;
+                    yield return RotateToFacePlayer();
+                    if(!_fired) {
+                        _animator.SetTrigger("Shoot");
+                        yield return new WaitForSeconds(shootAnim.length + delayBetweenVolleys);
+                    }
                 }
             }
-        }
+            public void ShootVolley() { // this is invoked by animation event
+                GameObject proj = Instantiate(projPrefab, projSpawnPos.position, projSpawnPos.rotation);
+                _itemsToCleanup.Add(proj);
+                _movementComponent.AddExternalVelocity(transform.forward * -knockbackForce);
+            }
+            
+            public void UnlockXRotation() => _lockXRotation = false;
+            public void Jump() { // this is invoked by animation event
+                _movementComponent.AddVerticalVelocity(jumpForce);
+            }
+            private IEnumerator RotateToFacePlayer() {
+                float timer=0;
+                while (Vector3.Angle(transform.forward, _player.transform.position - transform.position) > 10f){
+                    Vector3 targetDir = _player.transform.position - transform.position;
+                    float step = aimRotateSpeed * Time.deltaTime;
+                    Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+                    transform.rotation = Quaternion.LookRotation(newDir);
+                    Vector3 origRot = transform.eulerAngles;
+                    float x;
+                    if (_lockXRotation) {
+                        x = 0;
+                    } else {
+                        x = origRot.x;
+                    }
+                    transform.eulerAngles = new Vector3(x, transform.eulerAngles.y, origRot.z);
+                    yield return null;
+                    if (timer > -1) {
+                        timer += Time.deltaTime;
+                    }
+                    if (timer > maxAimTime) {
+                        _animator.SetTrigger("JumpShot");
+                        timer = -1;
+                        _fired = true;
+                    }
+                }
+                _lockXRotation = true;
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            }
+        #endregion
     }
-
 }
