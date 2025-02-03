ce8eebe738ec14f174fc8e373593803ed68d65d7
cu5tin <cu5tin@outlook.com>
2025-02-03 16:13:06 -0500

    refactored player health bar. added heart beat animation when at low health.

---
+++ Assets/Game/Scripts/UI/HeartsHealthBarNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Scripts {
+    [RequireComponent(typeof(Animator))]
+    public class HeartsHealthBarNode : MonoBehaviour {
+        [SerializeField] Image heartImage;
+        public bool isEnabled { get; private set; } = true;
+        private HealthComponent _healthComponent;
+        private Animator _animator;
+        
+        [SerializeField] private float lowHealthPercent = .3f;
+
+        private void Awake() {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void SetHealthComponent(HealthComponent healthComponent) {
+            _healthComponent = healthComponent;
+        }
+
+        public void UpdateLowHealthAnimation() {
+            _animator.SetBool("isLowHealth", _healthComponent.health / _healthComponent.maxHealth < lowHealthPercent);
+        }
+
+        
+        public void SetHeartEnabled(bool isEnabled) {
+            this.isEnabled = isEnabled;
+            heartImage.gameObject.SetActive(isEnabled);
+            //TODO: animate appearing or disappearing
+        }
+    }
+}
