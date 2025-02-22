c299d3e5ccc9efbd46584c368fc9a191dc3402a4
cu5tin <cu5tin@outlook.com>
2025-02-22 01:11:25 -0500

    Update PranimDriver.cs

--- Assets/Game/ProcAnim/PranimDriver.cs
+++ Assets/Game/ProcAnim/PranimDriver.cs
@@ -80,10 +80,6 @@ public class PranimDriver : MonoBehaviour
     #endregion
 
     #region materials
-    [SerializeField] private Material skin;
-    [SerializeField] private Material shirt;
-    [SerializeField] private Material pants;
-    [SerializeField] private Material hair;
     private Material[] _defaultMaterials;
     
     
