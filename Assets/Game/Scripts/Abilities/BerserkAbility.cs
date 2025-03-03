433e0f5b3223d878bf4802853249264129e4f6b4
SamuelWilensky <Samuelwilensky@gmail.com>
2025-03-03 14:53:36 -0500

    berserk change

--- Assets/Game/Scripts/Abilities/BerserkAbility.cs
+++ Assets/Game/Scripts/Abilities/BerserkAbility.cs
@@ -46,7 +46,7 @@ namespace Game.Scripts.Abilities {
 
         public void SetActive(float health)
         {
-            if (health <= hp.maxHealth / 2) //active
+            if (health < hp.maxHealth / 2) //active
             {
                 gfx.SetAllMaterialsToOneMat(berserkMaterial);
                 
